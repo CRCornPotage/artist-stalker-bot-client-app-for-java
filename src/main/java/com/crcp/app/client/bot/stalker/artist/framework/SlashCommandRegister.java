5d920c6192f1190c1979a52461030ee9e5934444
CRCornPotage <subaru20011107@gmail.com>
2025-05-07 05:42:52 +0900

    アノテーションベースに変更するため一旦もとのやつを削除

--- src/main/java/com/crcp/app/client/bot/stalker/artist/framework/SlashCommandRegister.java
+++ src/main/java/com/crcp/app/client/bot/stalker/artist/framework/SlashCommandRegister.java
@@ -1,92 +1,14 @@
 package com.crcp.app.client.bot.stalker.artist.framework;
 
-import java.util.List;
-
 import org.springframework.stereotype.Component;
 
-import com.crcp.app.client.bot.stalker.artist.constant.PropsConst;
-import com.crcp.app.client.bot.stalker.artist.framework.base.BaseOnReadyEventHandler;
-import com.crcp.app.client.bot.stalker.artist.framework.base.BaseSlashCommand;
-import com.crcp.app.client.bot.stalker.artist.framework.base.BaseSubCommand;
-import com.crcp.app.client.bot.stalker.artist.property.SpringProps;
-
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
-import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
-import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 
 @Component
-public class SlashCommandRegister extends BaseOnReadyEventHandler {
+public class SlashCommandRegister {
 	
-	private final SpringProps springProps;
-	private final List<BaseSlashCommand> slashCommands;
-	
-	public SlashCommandRegister(
-			SpringProps springProps,
-			List<BaseSlashCommand> slashCommands
-	) {
-		this.springProps = springProps;
-		this.slashCommands = slashCommands;
-	}
-	
-	@Override
 	public void execute(ReadyEvent event) {
-		JDA jda = event.getJDA();
-		switch (springProps.getProfiles().getActive()) {
-			case PropsConst.PRE_ENV -> this.runPreLogic(jda);
-			default -> this.runOtherLogic(jda);
-		}
+
 	}
-	
-	private void runPreLogic(JDA jda) {
-		jda
-				.updateCommands()
-				.addCommands(this.getCommandDatas());
-	}
-	
-	private void runOtherLogic(JDA jda) {
-		jda.getGuildCache().forEach(this::registerToGuild);
-	}
-	
-	private void registerToGuild(Guild guild) {
-		guild
-				.updateCommands()
-				.addCommands(this.getCommandDatas());
-	}
-	
-	private List<SlashCommandData> getCommandDatas() {
-		return this.slashCommands
-				.stream()
-				.map(this::getSlashCommandData)
-				.toList();
-	}
-	
-	private SlashCommandData getSlashCommandData(
-			BaseSlashCommand slashCommand
-	) {
-		final var slashCommandData = slashCommand.getSlashCommandData();
-		if (slashCommand.hasSubCommand()) {
-			slashCommandData.addSubcommands(
-					slashCommand
-							.getSubCommands()
-							.stream()
-							.map(this::getSubCommandData)
-							.toList()
-			);
-		} else if (slashCommand.hasOptions()) {
-			slashCommandData.addOptions(slashCommand.getOptions());
-		}
-		return slashCommandData;
-	}
-	
-	private SubcommandData getSubCommandData(
-			BaseSubCommand subCommand
-	) {
-		final var subCommandData = subCommand.getSubcommandData();
-		if (subCommand.hasOptions()) {
-			subCommandData.addOptions(subCommand.getOptions());
-		}
-		return subCommandData;
-	}
+
 }
--- src/main/java/com/crcp/app/client/bot/stalker/artist/framework/runner/OnReadyEventHandlersRunner.java
+++ src/main/java/com/crcp/app/client/bot/stalker/artist/framework/runner/OnReadyEventHandlersRunner.java
@@ -1,36 +1,13 @@
 package com.crcp.app.client.bot.stalker.artist.framework.runner;
 
-import java.util.List;
-
 import org.springframework.stereotype.Component;
-
-import com.crcp.app.client.bot.stalker.artist.framework.base.BaseOnReadyEventHandler;
 
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 
 @Component
 public class OnReadyEventHandlersRunner {
 	
-	private final List<BaseOnReadyEventHandler> onReadyEventHandlers;
-
-	public OnReadyEventHandlersRunner(
-			List<BaseOnReadyEventHandler> onReadyEventHandlers
-	) {
-		this.onReadyEventHandlers = onReadyEventHandlers;
-	}
-	
 	public void execute(ReadyEvent event) {
-		for (final var onReadyEventHandler : this.onReadyEventHandlers) {
-			// TODO 処理開始ログ(Info
-			try {
-				onReadyEventHandler.execute(event);
-			} catch (Exception e) {
-				// TODO エラー発生ログ(Error
-				// TODO 処理異常終了ログ(Info
-				throw e;
-			}
-			// TODO 処理正常終了ログ(Info
-		}
 	}
 	
 }
